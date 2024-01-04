65f5bea10ebcd533a2c0b6c7dd416d46941538c1
Edi Krasniqi <93128181+EdiKrasniqi11@users.noreply.github.com>
2024-01-04 14:32:53 +0100

    2024 update

--- src/Pages/CV/CV.js
+++ src/Pages/CV/CV.js
@@ -1,76 +1,19 @@
-import './CV.css'
-
+import "./CV.css";
 
 const CV = () => {
-    return(
-        <>
-            <div className="curriculumFotoDiv">
-                <h2 className="web-view curriculum"><a href="../">Home</a> {'>'} Curriculum Vitae</h2>
-                <h3 className="web-view curriculum">My experience in Programming</h3>
-            </div>
-            <div className="curriculumVitaeContainer">
-                <div className="leftSide">
-                    <p><b>Personal Information</b></p><br/>
-                    <p>Name</p>
-                    <p>E-mail</p><br/>
-                    <p><b>Education and Training</b></p><br/>
-                    <p>Mar,2023 - Present</p><br/><br/>
-                    <p>Nov.2022 - Mar,2023</p><br/><br/>
-                    <p>Nov,2020 - Present</p><br/><br/>
-                    <p>Sep,2017 - Jun,2020</p><br/><br/>
-                    <p>Feb,2022 - Jul,2022</p><br/><br/>
-                    <p>Aug,2018 - Oct,2018</p><br/><br/>
-                    <p>Mar,2017</p><br/><br/>
-                    <p>Aug,2017</p><br/><br/><br/>
-                    <p><b>Skills</b></p><br/><br/><br/><br/><br/><br/><br/>
-                    <p><b>Personal Skills <br/>and Competences</b></p><br/>
-                    <p>Skills</p><br/><br/><br/><br/><br/><br/>
-                    <p>Languages</p>
-                </div>
-                <div className="rightSide">
-                    <br/><br/>
-                    <p>Edi Krasniqi</p>
-                    <p>edikrasniqi1507@gmail.com</p>
-                    <br/><br/><br/>
-                    <p>Data Analyst</p>
-                    <p>“Ipko Telecommunications”, Prishtina, Kosovo</p><br/>  
-                    <p>Business Intelligence Internship</p>
-                    <p>“Ipko Telecommunications”, Prishtina, Kosovo</p><br/>  
-                    <p>University of Computer Science and Engineering</p>
-                    <p>"University of Business and Technology", Prishtina, Kosovo</p><br/>
-                    <p>High School</p>
-                    <p>Gymnasium of Natural Sciences "Hivzi Sulejmani", Fushe Kosove, Kosovo</p><br/>
-                    <p>ICT {'&'} Digital Skills Training on Java Enterprise and Mobile Programming</p>
-                    <p>"ICT for Kosovo's Growth", Kosovo</p><br/>
-                    <p>CCNA Routing and Switching: Introduction to Networks</p>
-                    <p>"American University of Kosovo Training and Development Institute", Prishtina, Kosovo</p><br/>
-                    <p>Lego EV3, Little Bits and Arduino Spring Camp Workshop</p>
-                    <p>"Shkolla Digjitale", Prishtina, Kosovo</p><br/>
-                    <p>Junior IT Computer Architecture, Network Security and Multimedia</p>
-                    <p>"Shkolla Digjitale", Prishtina, Kosovo</p><br/><br/><br/>
-                    <p>ReactJS</p>
-                    <p>C#</p>
-                    <p>JavaScript</p>
-                    <p>TypeScript</p>
-                    <p>PHP</p>
-                    <p>Python</p><br/><br/><br/><br/>
-                    <p>Leadership Skills</p>
-                    <p>Fast Learner</p>
-                    <p>Creative Problem Solving</p>
-                    <p>Software Design and Development</p>
-                    <p>Ability to Work Well with Others</p>
-                    <p>Project Management</p><br/>
-                    <p><b>Albanian</b></p>
-                    <p>Mother Tongue</p><br/>
-                    <p><b>English</b></p>
-                    <p>Fluent in Speaking and Writing</p>
-                </div>
-            </div>
-            <div className="downloadCVDiv">
-                    <a href="Edi Krasniqi CV.pdf" download><button className="downloadButton">Download CV</button></a>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="curriculumFotoDiv">
+        <h2 className="web-view curriculum">
+          <a href="../">Home</a> {">"} Curriculum Vitae
+        </h2>
+        <h3 className="web-view curriculum">My experience in Programming</h3>
+        <a href="Edi Krasniqi 2023 CV.pdf" download>
+          <button className="downloadButton">Download CV</button>
+        </a>
+      </div>
+    </>
+  );
+};
 
-export default CV
+export default CV;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,45 +1,68 @@
-import './Home.css'
-import React, { useRef } from 'react'
-import Edi from '../../Images/edi-profile.jpg'
-import emailjs from '@emailjs/browser'
+import "./Home.css";
+import React, { useRef } from "react";
+import Edi from "../../Images/edi-profile.jpg";
+import emailjs from "@emailjs/browser";
 
-
-const Home = () =>{
-    const form = useRef();
+const Home = () => {
+  const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_szb40ax', 'template_piaibdu', form.current, 'mT76i8kn0jPak2Rst')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_rmoyace",
+        "template_crm46y2",
+        form.current,
+        "stBk9Z0UscvXn8jsW"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
           e.target.reset();
-          alert("Thank You for Contacting me!")
-      }, (error) => {
+          alert("Thank You for Contacting me!");
+        },
+        (error) => {
           console.log(error.text);
-          alert("Something went wrong. In case of emergency contact please email me at edikrasniqi1507@gmail.com. Thank you for your patience.")
-      });
+          alert(
+            "Something went wrong. In case of emergency contact please email me at edikrasniqi1507@gmail.com. Thank you for your patience."
+          );
+        }
+      );
   };
-    return (
-        <>
-            <div id="homeFotoDiv">
-                <img src={Edi} alt="Profile Pic"/>
-                <h2>Hello, I'm Edi Krasniqi</h2>
-                <h3>And this is my Website</h3>
-            </div>
-            <div id="homeDiv">
-                <div id="contactDiv">
-                    <h2>Contact Me!</h2>
-                    <form ref={form} onSubmit={sendEmail} autoComplete="off">
-                      <input type="text" name="user_name" className="textInput" placeholder="Name"/><br/>
-                      <input type="email" name="user_email" className="textInput" placeholder="Email"/><br/>
-                      <textarea name="message" placeholder="Message"/><br/>
-                      <input type="submit" value="Send" className="submitButton"/>
-                    </form>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div id="homeFotoDiv">
+        <img src={Edi} alt="Profile Pic" />
+        <h2>Hello, I'm Edi Krasniqi</h2>
+        <h3>And this is my Website</h3>
+      </div>
+      <div id="homeDiv">
+        <div id="contactDiv">
+          <h2>Contact Me!</h2>
+          <form ref={form} onSubmit={sendEmail} autoComplete="off">
+            <input
+              type="text"
+              name="user_name"
+              className="textInput"
+              placeholder="Name"
+            />
+            <br />
+            <input
+              type="email"
+              name="user_email"
+              className="textInput"
+              placeholder="Email"
+            />
+            <br />
+            <textarea name="message" placeholder="Message" />
+            <br />
+            <input type="submit" value="Send" className="submitButton" />
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
